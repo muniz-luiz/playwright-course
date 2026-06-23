cda98fc51cdaebd8a1f1f7c4f2419efabf1ed646
Luiz Muniz <eusouluizeduardomuniz@gmail.com>
2026-06-23 09:01:50 -0300

    test: add loan navigation flow with back home option and validation

--- pages/loan_page.py
+++ pages/loan_page.py
@@ -4,6 +4,13 @@ from pages import login_page, home_page
 class LoanPage:
     def __init__(self, page: Page):
         self.page = page
+        # button/link that navigates back to the home page
+        self.back_to_home_page_button = page.get_by_role("link", name="Voltar para Home")
     
     def assert_on_loan_page(self):
         expect(self.page).to_have_url("**/loans.html")
+        
+    def back_to_home_page(self):
+        self.back_to_home_page_button.click()
+        self.page.wait_for_load_state("networkidle")  # Espera até que a página esteja completamente carregada
+        return home_page.HomePage(self.page)
--- tests/test_004_loan.py
+++ tests/test_004_loan.py
@@ -1,8 +1,12 @@
 from playwright.sync_api import Page, expect
-from pages import login_page, home_page, loan_page
+from pages.login_page import LoginPage
+from pages.home_page import HomePage
+from pages.loan_page import LoanPage
 
 
-def test_004_loan(page, login_page, home_page) -> None:
+
+
+def test_004_loan(page: Page, login_page: LoginPage, home_page: HomePage) -> None:
     login_page.login("user1", "pass1")
     #valido se fui para home page
     assert "home.html" in page.url
@@ -10,6 +14,10 @@ def test_004_loan(page, login_page, home_page) -> None:
     home_page.go_to_loan_page()
     #valido se estou na pagina de emprestimo
     assert "loans.html" in page.url
+    #clica no back to home 
+    loan_page = LoanPage(page)
+    home_page = loan_page.back_to_home_page()
+    assert "home.html" in page.url
     
     
     
