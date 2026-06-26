fcca5e8dfdf43c6e3f21ff6b5832f40d231c8140
Luiz Muniz <eusouluizeduardomuniz@gmail.com>
2026-06-26 16:54:01 -0300

    feat(test) implement pix flow with balance and statement validation

--- tests/test_003_make_pix.py
+++ tests/test_003_make_pix.py
@@ -2,6 +2,13 @@
 def test_003_make_pix(login_page, home_page) -> None:
     #vai logar na página inicial
     login_page.login("user1", "pass1")
+    login_page.assert_successful_login()
+    
+    #Ir para statement e pegar o saldo
+    statement_page = home_page.go_to_statement_page()
+    balance_before = statement_page.get_balance_value()
+    #Volta para o home
+    home_page = statement_page.back_to_home()
     #puxar e clicar no pix
     pix_page = home_page.go_to_pix_page()
     #valida se a página vai abrir
@@ -16,6 +23,16 @@ def test_003_make_pix(login_page, home_page) -> None:
     home_page = success_page.back_home_btn()
     #validar se voltou
     assert "home.html" in home_page.page.url
+    #click no ver extrato
+    statement_page = home_page.go_to_statement_page()
+    #Compara os valores
+    balance_after = statement_page.get_balance_value()
+    assert balance_after < balance_before
+    #volta para o home
+    home_page = statement_page.back_to_home()
+    back_login = home_page.back_to_login()
+    
+    
     
     
     
