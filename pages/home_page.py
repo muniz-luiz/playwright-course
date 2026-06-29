fad74678d0a200d0b2b36112c3b196579ebc1d94
Luiz Muniz <eusouluizeduardomuniz@gmail.com>
2026-06-29 17:44:07 -0300

    fix(test) stabilize pix flow navigation and resolve the timout issue using expect_navigation

--- pages/home_page.py
+++ pages/home_page.py
@@ -1,9 +1,4 @@
-from playwright.sync_api import Page
-from pages.loan_page import LoanPage
-from pages.statement_page import StatementPage
-from pages.pix_page import PixPage
-from pages.pay_bill import PayBillPage
-from pages.login_page import LoginPage
+from playwright.sync_api import Page, expect
 
 
 class HomePage:
@@ -17,26 +12,35 @@ class HomePage:
 
     
     def go_to_statement_page(self):
-        self.statement_button.click()
-        self.page.wait_for_url("**/statement.html")  # Espera até que a página esteja completamente carregada
+        from pages.statement_page import StatementPage
+        expect(self.statement_button).to_be_visible()
+        with self.page.expect_navigation(url="**/statement.html"):  # Espera até que a página statement esteja completamente carregada
+            self.statement_button.click()
         return StatementPage(self.page)    
     
     def go_to_pix_page(self):
-        self.pix_button.click()
-        self.page.wait_for_url("**/pix.html")  # Espera até que a página esteja completamente carregada
-        return PixPage(self.page)  
+        from pages.pix_page import PixPage
+        expect(self.pix_button).to_be_visible()
+        with self.page.expect_navigation(url="**/pix.html"):  # Espera até que a página pix esteja completamente carregada
+            self.pix_button.click()
+        return PixPage(self.page) 
         
     def go_to_paybills_page(self):
-        self.paybill_button.click()
-        self.page.wait_for_url("**/pay-bills.html")  # Espera até que a página esteja completamente carregada
+        from pages.pay_bill import PayBillPage
+        expect(self.paybill_button).to_be_visible()
+        with self.page.expect_navigation(url="**/pay-bills.html"):  # Espera até que a página pagar boletos esteja completamente carregada
+            self.paybill_button.click()
         return PayBillPage(self.page)
     
     def go_to_loan_page(self):
-        self.loan_button.click()
-        self.page.wait_for_url("**/loans.html")  # Espera até que a página esteja completamente carregada
+        from pages.loan_page import LoanPage
+        expect(self.loan_button).to_be_visible()
+        with self.page.expect_navigation(url="**/loans.html"): # Espera até que a página pagar loans esteja completamente carregada
+            self.loan_button.click()
         return LoanPage(self.page)  
     
     def back_to_login(self):
+        from pages.login_page import LoginPage
         self.logout_link.click()
         self.page.wait_for_url("**/login.html")
         return LoginPage(self.page)
